f3cfc9acd8cc1f178a7a43560442146cfa9bc24a
Traced <2020800023@qq.com>
2021-09-01 22:41:57 +0800

    优化

--- proxy/parser.go
+++ proxy/parser.go
@@ -1,33 +1,21 @@
 package proxy
 
 import (
-	"regexp"
-)
-
-var (
-	MatchHostPortRegexp = regexp.MustCompile(`(://)?([^:/]+)(:\d+)?`)
+	"net/url"
 )
 
 // HostParseToAddr 从 url 当中解析出域名跟端口
-// input : https://httpbin.org
-// output: httpbin.org:443
-//
-// input : http://httpbin.org:8080
-// output: httpbin.org:8080
-//
-// input : httpbin.org:8888
-// output: httpbin.org:8888
-func HostParseToAddr(host string) string {
-	r := MatchHostPortRegexp.FindAllStringSubmatch(host, -1)
-	switch len(r) {
-	case 1:
-		return r[0][0]
-	case 2:
-		if r[1][3] == "" {
-			return r[1][2] + ":80"
-		}
-		return r[1][2] + r[1][3]
-	default:
-		return host
+func HostParseToAddr(uri string) string {
+	var (
+		up, _      = url.Parse(uri)
+		host, port = up.Hostname(), up.Port()
+	)
+	if port != "" {
+		return host + ":" + port
 	}
+	port = "443"
+	if up.Scheme == "http" {
+		port = "80"
+	}
+	return host + ":" + port
 }
